e061dfdd194826a874596fdc448c108dbe775ce3
Gal Cohen <galvco@gmail.com>
2025-09-06 17:46:22 +0300

    fix(entitlements): register EntitlementTokenService provider in module

--- src/modules/entitlements/entitlements.module.ts
+++ src/modules/entitlements/entitlements.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { EntitlementsController } from './entitlements.controller';
 import { EntitlementsService } from './entitlements.service';
 import { PrismaModule } from '../../common/prisma/prisma.module';
+import { EntitlementTokenService } from '../../common/tokens/entitlement-token.service';
 
 @Module({
   imports: [PrismaModule],
   controllers: [EntitlementsController],
-  providers: [EntitlementsService],
+  providers: [EntitlementsService, EntitlementTokenService],
   exports: [EntitlementsService],
 })
 export class EntitlementsModule {}
